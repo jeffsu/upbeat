117c3f328ade1e18a9219b1a5668276713081587
Jeff Su <me@jeffsu.com>
2012-08-28 15:48:47 -0700

    adding arbitrary stats

--- www/public/javascripts/data.js
+++ www/public/javascripts/data.js
@@ -11,10 +11,8 @@ $(function () {
         $div.chart({
           template: type,
           values:   data,
-          legend: {
-            fail: "Failures",
-            pass: "Passes"
-          }
+          legend: getLegend(type, data),
+          series: getSeries(type, data)
         });
         setTimeout(function () { getData() }, 2000);
       });
@@ -22,4 +20,51 @@ $(function () {
 
     getData();
   });
+
+  var COLORS = [
+    "EFDECD",
+    "78DBE2",
+    "FFA474",
+    "FAE7B5",
+    "CB4154",
+    "1CD3A2",
+    "FFAACC",
+    "1DACD6",
+    "FFFF99",
+    "A2A2D0",
+    "000000"
+  ];
+
+  var COLOR_IDX = 0;
+
+  function getSeriesItem(color, width) {
+    return {
+      color: color || ('#' + COLORS[COLOR_IDX++%COLORS.length]),
+      "stroke-width": width === undefined ? 1 : width,
+      startAnimation: { type: 'reg', delay: 100, easing: 'elastic' }
+    };
+  }
+
+  function getLegend(type, data) {
+    var ret = {};
+    for (var key in data) ret[key] = key; 
+    return ret;
+  }
+
+  function getSeries(type, data) {
+    COLOR_IDX = 0;
+    var series = {};
+    if (type == 'lines') {
+      var i = 0;
+      for (var key in data) {
+        i++;
+        series[key] = getSeriesItem();
+      }
+    } else if (type = 'pass-fail') {
+      series.fail = getSeriesItem('red', 0);
+      series.pass = getSeriesItem('green', 2);
+    }
+    console.log(series);
+    return series;
+  }
 });
