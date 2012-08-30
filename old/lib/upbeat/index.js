00c0c17e5fd214d6c8d5118778d9c287b98fc904
Jeff Su <me@jeffsu.com>
2012-08-29 20:51:06 -0700

    adding time sensitivity to sensors

--- old/lib/upbeat/index.js
+++
@@ -1,9 +0,0 @@
-require('mochiscript');
-
-var Action      = require('./action');
-exports.Action  = Action;
-exports.Service = require('./service');
-exports.Server  = require('./server');
-exports.Config  = require('./config');
-
-exports.registerStrategy = function (name, req) { Action.registerStrategy(name, req) };
