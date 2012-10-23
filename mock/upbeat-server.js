6d33ea70d63a5cc23947907a8eb761630d96e63b
Jeff Su <me@jeffsu.com>
2012-10-23 14:53:18 -0700

    adding max sockets to prevent memory leak

--- mock/upbeat-server.js
+++ mock/upbeat-server.js
@@ -1,6 +1,7 @@
 require('mochiscript');
 require('js-yaml');
 
+require('http').globalAgent.maxSockets = 100;
 var upbeat = require('../lib/upbeat');
 var config = require('./example.yml');
 var server = new upbeat.Server(config);
