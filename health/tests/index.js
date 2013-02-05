04900b4f5a91a6cd594ded96407dd8d72cfe5763
vipworld <vipworld@gmail.com>
2013-02-05 16:23:09 +0800

    fix redis strategy and add test

--- health/tests/index.js
+++ health/tests/index.js
@@ -1,3 +1,3 @@
 require('mochiscript');
 var reporter = require('nodeunit').reporters.default;
-reporter.run([ 'tests/checker.ms', 'tests/http.ms', 'tests/tcp.ms' ]);
+reporter.run([ 'tests/checker.ms', 'tests/http.ms', 'tests/tcp.ms', 'tests/redis.ms' ]);
