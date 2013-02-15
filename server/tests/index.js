9ebb21d51b73340403216546ec8e3502a36cd32f
vipworld <vipworld@gmail.com>
2013-02-15 13:01:47 +0800

    pass interval option from server

--- server/tests/index.js
+++ server/tests/index.js
@@ -1,5 +1,3 @@
 require('mochiscript');
 var reporter = require('nodeunit').reporters.default;
-//reporter.run([ 'tests/sensor.ms', 'tests/service.ms', 'tests/stats.ms' ]);
-//reporter.run([ 'tests/sensor.ms', 'tests/service.ms' ]);
-reporter.run([ 'tests/sensor.ms', 'tests/service.ms'  ]);
+reporter.run([ 'tests/service.ms', 'tests/sensor.ms' ]);
